f2fdf962f35e79958626c9303feb0dd507f54316
Terry Wang <terrywang@tianyu.ninja>
2018-06-17 01:54:22 -0400

    firebase setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
+import { firebaseApiKey, firebaseAuthDomain, firebaseDatabaseURL, firebaseProjectId, firebaseStorageBucket, firebaseMessagingSenderId } from 'react-native-dotenv';
 import { Header } from './components/common';
 
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: firebaseApiKey,
+      authDomain: firebaseAuthDomain,
+      databaseURL: firebaseDatabaseURL,
+      projectId: firebaseProjectId,
+      storageBucket: firebaseStorageBucket,
+      messagingSenderId: firebaseMessagingSenderId
+    });
+  }
+
   render() {
     return (
       <View>
